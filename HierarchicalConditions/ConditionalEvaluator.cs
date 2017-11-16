effe088ff5e29a3a4813d94a5a266b54e00b45ff
Omkar P <omkar.patekar@gmail.com>
2017-11-16 23:46:31 +1100

    Add Initial commit

---
+++ HierarchicalConditions/ConditionalEvaluator.cs
@@ -0,0 +1,147 @@
+﻿/// <summary>
+/// 
+/// </summary>
+namespace HierarchicalConditions
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// ConditionalEvaluator simply replaces if/else with organised constructs.
+    /// Code becomes readable and maintainable.
+    /// </summary>
+    public class ConditionalEvaluator
+    {
+        /// <summary>
+        /// Basic condition types
+        /// </summary>
+        public enum Condition
+        {
+            And = 0,
+            Or = 1,
+            Not = 2
+        }
+
+        List<ConditionalEvaluator> ce = new List<ConditionalEvaluator>();
+
+        public ConditionalEvaluator Also { get; private set; }
+        Condition conditionType = Condition.And;
+        bool condition = false;
+        Func<int> truthyFunc = null;
+        Func<int> falsyFunc = null;
+
+        public ConditionalEvaluator(bool condition, Func<int> truthyFunc = null, Func<int> falsyFunc = null, Condition conditionType = Condition.And)
+        {
+            Also = this;
+            this.condition = condition;
+            this.truthyFunc = truthyFunc;
+            this.falsyFunc = falsyFunc;
+            this.conditionType = conditionType;
+        }
+
+        public ConditionalEvaluator And(bool condition, Func<int> truthyFunc = null, Func<int> falsyFunc = null)
+        {
+            ConditionalEvaluator tAnd =
+                    new ConditionalEvaluator(condition, truthyFunc, falsyFunc, Condition.And);
+            tAnd.Also = this;
+            ce.Add(tAnd);
+            return tAnd;
+        }
+
+        public ConditionalEvaluator Or(bool condition, Func<int> truthyFunc = null, Func<int> falsyFunc = null)
+        {
+            ConditionalEvaluator or =
+                new ConditionalEvaluator(condition, truthyFunc, falsyFunc, Condition.Or);
+            or.Also = this;
+            ce.Add(or);
+            return or;
+        }
+
+        public ConditionalEvaluator Untrue(bool condition, Func<int> truthyFunc)
+        {
+            ConditionalEvaluator not =
+                new ConditionalEvaluator(condition, truthyFunc, null, Condition.Not);
+            not.Also = this;
+            ce.Add(not);
+            return not;
+        }
+
+        public ConditionalEvaluator Untrue(Func<int> truthyFunc)
+        {
+            ConditionalEvaluator not =
+                new ConditionalEvaluator(true, truthyFunc, null, Condition.Not);
+            not.Also = this;
+            ce.Add(not);
+            return not;
+        }
+
+        public void Evaluate(bool preConditions = true)
+        {
+            bool FuncsRun = false;
+            switch (this.conditionType)
+            {
+                case Condition.Or:
+                    if (preConditions)
+                    {
+                        try { truthyFunc(); } catch (Exception) { }
+                        FuncsRun = true;
+                    }
+                    break;
+                case Condition.And:
+                    if (preConditions == false)
+                    {
+                        try { falsyFunc(); } catch (Exception) { }
+                        FuncsRun = true;
+                    }
+                    break;
+                case Condition.Not:
+                    if (preConditions == true)
+                    {
+                        try { falsyFunc(); } catch (Exception) { }
+                        FuncsRun = true;
+                    }
+                    break;
+            }
+
+            if (!FuncsRun)
+            {
+                RunFuncs();
+            }
+
+            switch (this.conditionType)
+            {
+                case Condition.Or:
+                    preConditions = preConditions | this.condition;
+                    break;
+                case Condition.And:
+                    preConditions = preConditions & this.condition;
+                    break;
+                case Condition.Not:
+                    preConditions = !preConditions;
+                    break;
+            }
+
+            EvaluateSubConditions(preConditions);
+        }
+
+        private void RunFuncs()
+        {
+            if (this.condition)
+            {
+                try { truthyFunc(); } catch (Exception) { }
+            }
+            else
+            {
+                try { falsyFunc(); } catch (Exception) { }
+            }
+        }
+
+        private void EvaluateSubConditions(bool preCondition)
+        {
+            foreach (var condition in ce)
+            {
+                condition.Evaluate(preCondition);
+            }
+        }
+    }
+}
---
+++ HierarchicalConditions/Program.cs
@@ -0,0 +1,71 @@
+﻿///
+/// 
+
+namespace HierarchicalConditions
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] flags = new bool[] { true, false, true, true, false, false };
+            ConditionalEvaluator ce = new ConditionalEvaluator
+                (flags[0], GetFunc("0 true"), GetFunc("0 false"));
+            ce
+                .And(flags[1], GetFunc("0 Or 1 true"), GetFunc("0 Or 1 false"))
+                    .Untrue(flags[2], GetFunc("True only on (0 Or 1) == false"));
+            ce
+                .Or(flags[3] || flags[4], GetFunc("0 AND 3 true"), GetFunc("0 AND 3 false"))
+                    .Untrue(GetFunc("True only on (0 AND 3) == false"));
+
+            // The above code replaces
+            //if (flags[0])
+            //{ Console.WriteLine("0 true"); }
+            //else { Console.WriteLine("0 false"); }
+
+            //if (flags[0] || flags [1])
+            //{ Console.WriteLine("0 Or 1 true"); }
+            //else { Console.WriteLine("0 Or 1 false"); }
+
+            //if ((!(flags[0] || flags [1])) && (flags[2]))
+            //{ Console.WriteLine("True only on (0 Or 1) == false"); }
+
+            //if (flags[0] && (flags[3] || flags[4]))
+            //{ Console.WriteLine("0 AND 3 true"); }
+            //else { Console.WriteLine("0 AND 3 false"); }
+
+            //if (!(flags[0] && (flags[3] || flags[4])))
+            //{ Console.WriteLine("True only on(0 AND 3) == false"); }
+
+            Console.WriteLine("Press ESC key to quit, any other key to see demo");
+            ConditionalEvaluator ce2 = new ConditionalEvaluator(
+                Console.ReadKey().Key != ConsoleKey.Escape,             // Condition
+                () => { ce.Evaluate(); Console.ReadKey(); return 0; },  // If
+                GetFunc("Non-Enter pressed"));                          // Else
+            ce2.Evaluate();
+
+            // The above block replaces
+            if (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                ce.Evaluate();
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Non-Enter pressed");
+            }
+        }
+
+        static Func<int> GetFunc(string msg, Func<int> postAction = null)
+        {
+            Func<int> FuncWithMessage = () =>
+            {
+                Console.WriteLine(msg);
+                postAction?.Invoke();
+                return 0;
+            };
+            return FuncWithMessage;
+        }
+    }
+}
